bde72234d6aedcc449eb71e38b415b3ad8400a5e
lakecrimsonn <lakecrimsonn@gmail.com>
2023-10-14 18:18:24 +0900

    mission completed

--- App.js
+++ App.js
@@ -1,12 +1,148 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  ScrollView,
+  Text,
+  StyleSheet,
+  Dimensions,
+  ActivityIndicator,
+} from "react-native";
+import * as Location from "expo-location";
+import { Fontisto } from "@expo/vector-icons";
+
+const { height: SCREEN_HEIGHT, width: SCREEN_WIDTH } = Dimensions.get("window");
+const weatherAPI = "####";
+
+const icons = {
+  Clouds: "cloudy",
+  Rain: "rain",
+  Clear: "day-sunny",
+  Atmosphere: "cloudy-gusts",
+  Snow: "snow",
+  Drizzle: "rain",
+  Thunderstorm: "lightning",
+};
 
 export default function App() {
+  const [city, setCity] = useState("Loading...");
+  const [ok, setOk] = useState(true);
+  const [days, setDays] = useState([]);
+
+  const getWeather = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+
+    if (!granted) {
+      setOk(false);
+    }
+
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+
+    const getLocation = await Location.reverseGeocodeAsync(
+      { latitude, longitude },
+      { useGoogleMaps: false }
+    );
+
+    setCity(getLocation[0].city);
+
+    const response = await fetch(
+      `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude={alerts}&appid=${weatherAPI}&units=metric`
+    );
+    const json = await response.json();
+    setDays(json.daily);
+  };
+
+  useEffect(() => {
+    getWeather();
+  }, []);
+
   return (
-    <View style={{ flex: 1, flexDirection: "row" }}>
-      <View style={{ flex: 1, backgroundColor: "tomato" }}></View>
-      <View style={{ flex: 3, backgroundColor: "teal" }}></View>
-      <View style={{ flex: 1, backgroundColor: "orange" }}></View>
+    <View style={styles.container}>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>{city}</Text>
+      </View>
+      <ScrollView
+        pagingEnabled
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.weather}
+      >
+        {days.length === 0 ? (
+          <View style={{ ...styles.day, alignItems: "center" }}>
+            <ActivityIndicator
+              color={"white"}
+              size={"large"}
+              style={{ marginTop: 10 }}
+            />
+          </View>
+        ) : (
+          days.map((day, index) => (
+            <View key={index} style={styles.day}>
+              <Text style={styles.tiny_description}></Text>
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  justifyContent: "space-between",
+                  width: "100%",
+                }}
+              >
+                <Text style={styles.temp}>
+                  {parseFloat(day.temp.day).toFixed(1)}
+                </Text>
+                <Fontisto
+                  name={icons[day.weather[0].main]}
+                  size={68}
+                  color="white"
+                />
+              </View>
+              <Text style={styles.description}>{day.weather[0].main}</Text>
+              <Text style={styles.tiny_description}>
+                {day.weather[0].description}
+              </Text>
+            </View>
+          ))
+        )}
+      </ScrollView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "tomato",
+  },
+  city: {
+    flex: 1.1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    fontSize: 48,
+    fontWeight: "500",
+    color: "white",
+  },
+  weather: {},
+  day: {
+    width: SCREEN_WIDTH,
+    alignItems: "flex-start",
+    paddingHorizontal: 20,
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 108,
+    color: "white",
+  },
+  description: {
+    marginTop: -30,
+    fontSize: 58,
+    color: "white",
+  },
+  tiny_description: {
+    fontSize: 28,
+    color: "white",
+    marginTop: -5,
+  },
+});
